3056548346b00c6d23cfcfd4a99ab8b034543201
keepsixok <ad1oz.gaming@gmail.com>
2023-09-09 10:29:56 -0600

    update projects

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import { AiOutlineFacebook, AiOutlineGithub, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineTwitter } from 'react-icons/ai'
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='text-center'>
+        <div id='name-card-title' className='text-center pt-8 font-bold'>
+            <h1 className=' text-4xl uppercase'>Mitchell</h1>
+            <h1 className='font-medium'>Web Developer</h1>
+            <div className='flex w-[60%] mx-auto justify-between items-center py-8 md:w-[30%]'>
+                 <a href=""><AiOutlineFacebook  size={40} /></a>
+                 <a href=""><AiOutlineInstagram size={40} /></a>
+                 <a href=""><AiOutlineTwitter size={40} /></a>
+                 <a href=""><AiOutlineLinkedin size={40} /></a>
+                 <a href=""><AiOutlineGithub size={40} /></a>
+
+            </div>
+        </div>
+
+        <div>
+            <h1 className='text-4xl font-bold uppercase pt-8'>Projects</h1>
+        </div>
+        
+    </div>
   )
 }
 
--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Avatar from './components/Avatar'
+import Hero from './components/Hero'
+import Projects from './components/Projects'
 
 export default function Home() {
   return (
     <div>
       <Avatar />
+      <Hero />
+      <Projects />
     </div>
   )
 }
