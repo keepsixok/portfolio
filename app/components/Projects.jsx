a72a2be8b37a9a92a5e48e350d8d2b73ea18fa4e
keepsixok <ad1oz.gaming@gmail.com>
2023-09-10 22:19:09 -0600

    update

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -13,19 +13,22 @@ const Projects = () => {
           <h1>Open Source Chef</h1>
         </div>
       
-      
+      <div className='flex flex-wrap justify-center py-4'>
       <div className='flex items-center bg-green-300'>
           <h1 className='p-4'>Recipe Database</h1>
         </div>
-        <div className='flex items-center '>
-          <h1 className='pl-4'>Recipe Scaling</h1>
+        <div className='flex items-center bg-blue-200'>
+          <h1 className='p-4'>Recipe Scaling</h1>
         </div>
-        <div className='flex items-center '>
-          <h1 className='pl-4'>Prep Lists</h1>
+        <div className='flex items-center bg-pink-300 '>
+          <h1 className='p-4'>Prep Lists</h1>
         </div>
-        <div className='flex items-center'>
-          <h1 className='pl-4'>Food Costing</h1>
+        <div className='flex items-center bg-yellow-200'>
+          <h1 className='p-4'>Food Costing</h1>
         </div>
+        </div>
+
+
       </div>
       
 
