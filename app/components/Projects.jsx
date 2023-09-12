c1f53ec6ad9444f5c3b80b4b1c06d4dd979f6033
keepsixok <ad1oz.gaming@gmail.com>
2023-09-11 22:11:15 -0600

    update projects

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -4,28 +4,18 @@ const Projects = () => {
 
     <div className='flex flex-wrap justify-center w-full px-4'>
         <div>
-            <h1 className='text-4xl font-bold  pt-8'>Tasks</h1>
+            <h1 className='text-4xl font-bold pb-4   pt-8'>Tasks</h1>
         </div>
 
        <div className='flex flex-wrap w-full h-48 border border-black rounded-2xl shadow-lg shadow-gray-300 '>
 
-        <div className='flex justify-center items-center border-b border-black w-full' >
+        <div className='flex justify-center items-center border-b border-black h-12 w-full' >
           <h1>Open Source Chef</h1>
         </div>
       
-      <div className='flex flex-wrap justify-center py-4'>
-      <div className='flex items-center bg-green-300'>
-          <h1 className='p-4'>Recipe Database</h1>
-        </div>
-        <div className='flex items-center bg-blue-200'>
-          <h1 className='p-4'>Recipe Scaling</h1>
-        </div>
-        <div className='flex items-center bg-pink-300 '>
-          <h1 className='p-4'>Prep Lists</h1>
-        </div>
-        <div className='flex items-center bg-yellow-200'>
-          <h1 className='p-4'>Food Costing</h1>
-        </div>
+      <div className='flex h-2 border-b border-green-600 w-full'>
+        <h1>test</h1>
+       
         </div>
 
 
